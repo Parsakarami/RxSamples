d4fb7c7612e22d150aa0e61a0ec4c17875e715e3
Parsa <karami.parsa.1994@gmail.com>
2023-01-05 17:23:43 -0500

    Change ReactiveUI packages versions to be compatible with .NET5, Add the StepInterval View and ViewModel

---
+++ Samples/StepInterval/StepIntervalView.xaml.cs
@@ -0,0 +1,37 @@
+﻿using ReactiveUI;
+using System.Reactive.Disposables;
+using System.Windows;
+
+namespace RxSamples.Samples.StepInterval
+{
+    public partial class StepIntervalView : Window, IViewFor<StepIntervalViewModel>
+    {
+        public StepIntervalView()
+        {
+            InitializeComponent();
+            ViewModel = new StepIntervalViewModel();
+            this.WhenActivated(disposable =>
+            {
+                this.Bind(ViewModel,
+                    vm => vm.Count,
+                    v => v.ValueLabel.Content)
+                .DisposeWith(disposable);
+            });
+        }
+
+        #region ViewModel Properties
+        public StepIntervalViewModel ViewModel { get; set; }
+
+        object IViewFor.ViewModel
+        {
+            get => ViewModel;
+            set => ViewModel = (StepIntervalViewModel)value;
+        }
+        StepIntervalViewModel IViewFor<StepIntervalViewModel>.ViewModel
+        {
+            get => ViewModel;
+            set => ViewModel = value;
+        }
+        #endregion
+    }
+}
---
+++ Samples/StepInterval/StepIntervalViewModel.cs
@@ -0,0 +1,32 @@
+﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RxSamples.Samples.StepInterval
+{
+    public class StepIntervalViewModel : ReactiveObject, IActivatableViewModel
+    {
+        public ViewModelActivator Activator { get; set; } = new ViewModelActivator();
+        [Reactive]
+        public int Count { get; set; } = 0;
+        public StepIntervalViewModel()
+        {
+            this.WhenActivated(disposable =>
+            {
+                Observable
+                .Interval(TimeSpan.FromMilliseconds(250),RxApp.MainThreadScheduler)
+                .Subscribe(_ =>
+                {
+                    Count++;
+                }).DisposeWith(disposable);
+            });
+        }
+    }
+}
